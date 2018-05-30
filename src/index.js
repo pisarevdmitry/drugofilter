f7cfcd5f4c67830059d87d8769eb6e162deec8c1
dmitry <pisarev_dmitry@mail.ru>
2018-05-30 10:53:21 +0300

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import './styles'
 
 import FriendListDrag from './modules/friendListsDrag'
-import startApp from './modules/startApp'
 import filterFriends from './modules/filterFriends'
-import closeApp from './modules/closeApp'
-
+import App from './modules/App';
 const initApp = document.querySelector('#init-app');
 const allFriendsSearch = document.querySelector('#allfriends-search');
 const chosenFriendsSearch = document.querySelector('#choosenfriends-search');
 const allFriendsResult = document.querySelector('#allFriends-result');
 const chosenFriendResult = document.querySelector('#chosenFriends-result');
 
- startApp({
+App({
     API_KEY: 5350105,
     vkParam: 2,
     responseVersion: '5.76',
@@ -32,4 +30,3 @@ new FriendListDrag({
         container: 'friend-list',
     }
 });
-closeApp();
---
+++ src/modules/App.js
@@ -0,0 +1,105 @@
+import VKAuth from './Auth'
+
+class App extends VKAuth {
+    constructor(param) {
+        super(param);
+        let {initApp} = param;
+        this.initApp = initApp;
+        this.allFriends = {};
+        this.chosenFriends = {};
+        this.wrapper =  document.querySelector('.wrapper');
+        this.app =  document.querySelector(".App");
+        this.saveBtn = document.querySelector('#btn_save');
+        this.closeBtn = document.querySelector('#btn-close');
+        this.templateAllFriends = document.querySelector('#allFriends').textContent;
+        this.templateChosenFriends = document.querySelector('#chosenFriends').textContent;
+        this.allFriendsResult = document.querySelector('#allFriends-result');
+        this.chosenFriendResult = document.querySelector('#chosenFriends-result');
+        this.startApp = this.startApp.bind(this);
+        this.popup = document.querySelector('.popup');
+        this.closeApp = this.closeApp.bind(this);
+        this.saveData = this.saveData.bind(this);
+        this.closePopup = this.closePopup.bind(this);
+        this.initListener()
+    }
+    initListener() {
+        this.initApp.addEventListener('click', this.startApp)
+        this.saveBtn.addEventListener('click', this.saveData);
+        this.closeBtn.addEventListener('click', this.closeApp);
+        this.popup.addEventListener('click', this.closePopup)
+    }
+    showApp() {
+        this.wrapper.classList.add('overlay');
+        this.app.classList.remove('hidden');
+        this.initApp.classList.add('hidden');
+    }
+    async startApp(e){
+        e.preventDefault();
+        if (localStorage.getItem('myFriends')) {
+            let {allFriends, chosenFriends} = JSON.parse(localStorage.getItem('myFriends'));
+            this.allFriends = allFriends;
+            this.chosenFriends = chosenFriends;
+            this.showApp();
+            this.renderFriends();
+        } else {
+            try {
+                await this.auth();
+                this.allFriends = await this.callApi('friends.get', {fields: 'photo_100'});
+                this.showApp();
+                this.allFriends.items.forEach((friend) => friends.set(friend.id, friend));
+                this.renderFriends();
+
+            } catch (e) {
+                console.log(e.message)
+            }
+        }
+    }
+    renderFriends() {
+        const renderAllFriends = Handlebars.compile(this.templateAllFriends);
+        const renderChosenFriends = Handlebars.compile(this.templateChosenFriends);
+        const allFriendsHtml = renderAllFriends(this.allFriends);
+        const chosenFriendsHtml = renderChosenFriends(this.chosenFriends);
+
+        this.allFriendsResult.innerHTML = allFriendsHtml;
+        this.chosenFriendResult.innerHTML = chosenFriendsHtml
+    }
+    saveData() {
+        let allFriends, chosenFriends;
+
+        allFriends = this.composeFriends(this.allFriendsResult);
+        chosenFriends = this.composeFriends(this.chosenFriendResult);
+        localStorage.setItem('myFriends', JSON.stringify({ allFriends, chosenFriends }));
+        this.popup.classList.remove('hidden');
+    }
+    composeFriends(list) {
+        let arr= [];
+
+        for (let child of list.children) {
+            let [firstName, lastName] = child.querySelector('.friends-list__name').innerText.split(' ');
+            let photo100 = child.querySelector('.friends-lists__picture').getAttribute('src');
+
+            arr.push({
+                first_name: firstName, last_name: lastName, photo_100: photo100
+            })
+        }
+
+        return {
+            items: arr
+        }
+    }
+    closeApp() {
+        this.wrapper.classList.remove('overlay');
+        this.app.classList.add('hidden');
+        this.initApp.classList.remove('hidden');
+    }
+    closePopup(e) {
+        if (e.target.tagName === 'BUTTON') {
+            this.popup.classList.add('hidden');
+            this.closeApp()
+        }
+    }
+}
+
+export default (param) => {
+    new App(param)
+}
